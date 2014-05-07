0d73f1606dabb1bdd28ea66b169ea396225d649c
Papaja <papaja@papaja.no-ip.org>
2014-05-07 10:49:13 +0200

    Refactoring Store, Source and Backup

---
+++ source.py
@@ -0,0 +1,184 @@
+__author__ = 'papaja'
+
+import pickle
+import os
+import hashlib
+from stat import *
+from backup_lib import BackupObject
+from backup_lib import verbose
+from backup_lib import objects_init
+
+class SourceObject(BackupObject):
+
+    @staticmethod
+    def create(source_path, store, target_object):
+        lstat = os.lstat(source_path)
+        mode = lstat.st_mode
+        if S_ISDIR(mode):
+                return SourceDir(source_path, store, lstat, target_object)
+        elif S_ISREG(mode):
+                return SourceFile(source_path, store, lstat, target_object)
+        elif S_ISLNK(mode):
+                return SourceLnk(source_path, store, lstat, target_object)
+        else:
+                # Unknown file
+                return None
+
+    def __init__(self, source_path, store, lstat, target_object):
+        #if objects_init : print "Initializing SourceFile (%s)" % source_path
+        BackupObject.__init__(self, source_path, store, lstat)
+        self.target_object = target_object
+
+
+    def exist_backup(self):
+        file_hash = self.store.get_hash(self.source)
+        return os.path.exists(self.store.get_object_path()) ####################################
+
+    def compare_stat(self, object_stat, backuped_stat):
+        return (object_stat.st_size == backuped_stat.st_size and #nezmenil velkost
+                object_stat.st_uid == backuped_stat.st_uid and # nezmenil uziv
+                object_stat.st_gid == backuped_stat.st_gid and # nezmenil skupinu
+                object_stat.st_mtime == backuped_stat.st_mtime and # posledna modifikacia
+                object_stat.st_mode == backuped_stat.st_mode and
+                object_stat.st_ctime == backuped_stat.st_ctime) # last metadata change time
+
+
+class SourceFile(SourceObject):
+
+    def __init__(self, source_path, store, lstat, target_object):
+        if objects_init : print("Initializing SourceFile (%s)") % source_path
+        #print source_path
+        SourceObject.__init__(self, source_path, store, lstat, target_object)
+
+    def save_file(self):
+        return self.store.save_file(self.source_path, self.name)
+
+    #REFACTORED
+    def backup(self):
+        # ak sa zmenil mtime, tak ma zmysel pozerat sa na obsah suboru
+        # inak sa mozno zmenili zaujimave metadata
+        if self.target_object != None:
+            if not self.compare_stat(self.lstat, self.target_object.lstat): # ak sa nerovnaju lstaty
+                if (self.lstat.st_mtime == self.target_object.lstat.st_mtime
+                    and self.lstat.st_size == self.target_object.lstat.st_size):
+                    if verbose : print("Lnk mTime bez zmeny. return novy side_dict(stary_hash) !")
+                    # rovanky mtime
+                    # vyrob side dict stary hash + aktualny lstat
+                    return self.make_side_dict(self.target_object.side_dict['hash']) #stary hash
+                else:
+                    # rozny mtime
+                    new_hash = self.store.get_hash(self.source_path) # spocitaj hash a porovnaj
+                    if (new_hash == self.target_object.side_dict['hash']
+                        or os.path.exists(self.store.get_object_path(new_hash))):
+                        if verbose : print("File mTime zmeneny. return novy side_dict(novy_hash) !")
+                        return self.make_side_dict(new_hash)
+                    else:
+                        if verbose : print("File Novy object zalohy.")
+                        hash = self.save_file()
+                        return self.make_side_dict(hash)
+            else:
+                if verbose : print("Lnk mTime zmeneny. rovnake meta")
+                return self.target_object.side_dict # ak sa rovnaju staty
+        else:
+            if verbose : print("File Novy object zalohy.")
+            hash = self.save_file()
+            return self.make_side_dict(hash)
+
+
+class SourceDir(SourceObject):
+
+    def __init__(self, source_path, store, lstat, target_object):
+        if objects_init : print("Initializing SourceDir (%s)") % source_path
+        #print source_path
+        SourceObject.__init__(self, source_path, store, lstat, target_object)
+        if self.target_object != None: print(self.target_object.side_dict)
+
+    #REFACTORED
+    def pickling(self, input_dict):
+        pi = pickle.dumps(input_dict)
+        hash_name = hashlib.sha1(pi).hexdigest()
+        if (self.target_object == None
+            or not os.path.exists(self.store.get_object_path(hash_name))): #or ...hashe sa nerovnaju...:
+            with self.store.get_object_file(hash_name, "wb") as DF:
+                with self.store.get_object_file_header(hash_name, "wb") as DHF:
+                    DHF.write("directory\n")
+                    DF.write(pi)
+                    DF.close()
+                    DHF.close()
+        return hash_name
+
+    def backup(self):
+        #Metoda SourceDir.incremental_backup() je zmatocna.
+        #Ak neexistuje self.target_object (teda stara cielova verzia aktualneho adresara),
+        #metodu initial_backup() treba volat na podobjekt v adresari
+        #(vytvoreny pomocou SourceObject.create(next_path,self.target,None)),
+        #nie na (self teda na aktualny adresar). Takto spraveny incremental_backup()
+        #bude potom v pripade neexistujuceho target_object fungovat rovnako
+        #ako initial_backup() a teda nemusite mat dve metody
+        #(ale podobne treba spravit aj incremental_backup() v SourceFile a SourceLnk).
+        main_dict = {}
+        for F in os.listdir(self.source_path):
+                next_path = os.path.join(self.source_path,F)
+                if self.target_object != None:
+                    oldF = self.target_object.get_object(F)
+                else:
+                    oldF = None
+                new_object = SourceObject.create(next_path, self.store, oldF)
+                side_dict = new_object.backup()
+                main_dict[F] = side_dict
+        #print main_dict
+        hash = self.pickling(main_dict)
+        return self.make_side_dict(hash)
+
+class SourceLnk(SourceObject):
+
+    def __init__(self, source_path, store, lstat, target_object):
+        if objects_init : print("Initializing SourceLnk (%s)") % source_path
+        #print source_path
+        SourceObject.__init__(self, source_path, store, lstat, target_object)
+
+    #REFACTORED
+    def make_lnk(self):
+        link_target = os.readlink(self.source_path)
+        hash_name = hashlib.sha1()
+        hash_name.update(link_target)
+        with self.store.get_object_file(hash_name.hexdigest(), "wb") as DF:
+            with self.store.get_object_file_header(hash_name.hexdigest(), "wb") as DHF:
+                DHF.write("link\n")
+                DHF.write("signature\n")
+                DHF.write(str(0))
+                DHF.write("\n")
+                DF.write(link_target)
+                DHF.close()
+            DF.close()
+        return hash_name.hexdigest()
+
+    #def initial_backup(self):
+    #            return self.make_side_dict(self.make_lnk())
+
+    def backup(self):
+        if self.target_object != None:
+            if not self.compare_stat(self.lstat, self.target_object.lstat): # ak sa nerovnaju lstaty
+                if (self.lstat.st_mtime == self.target_object.lstat.st_mtime
+                    and self.lstat.st_size == self.target_object.lstat.st_size):
+                    if verbose : print("Lnk mTime bez zmeny. return novy side_dict(stary_hash) !")
+                    # rovanky mtime
+                    # vyrob side dict stary hash + aktualny lstat
+                    return self.make_dict(self.target_object.side_dict[self.name]['hash']) #stary hash
+                else:
+                    # rozny mtime
+                    link_target = os.readlink(self.source_path)
+                    new_hash = hashlib.sha1(link_target).hexdigest() # spocitaj hash a porovnaj
+                    if (new_hash == self.target_object.side_dict[self.name]['hash']
+                        or os.path.exists(self.store.get_object_path(new_hash))):
+                        if verbose : print("Lnk mTime zmeneny. return novy side_dict(novy_hash) !")
+                        return self.make_dict(new_hash)
+                    else:
+                        if verbose : print("Lnk Novy object zalohy !")
+                        return self.make_side_dict(self.make_lnk())
+            else:
+                if verbose : print("Lnk mTime zmeneny. rovnake meta")
+                return self.target_object.side_dict # ak sa rovnaju staty
+        else:
+            if verbose : print("Lnk Novy object zalohy.")
+            return self.make_side_dict(self.make_lnk())
---
+++ store.py
@@ -0,0 +1,302 @@
+__author__ = 'papaja'
+
+import pickle
+import os
+import hashlib
+import gzip
+import constants
+from stat import *
+from backup_lib import BackupObject
+from backup_lib import objects_init
+
+class Store():
+
+    def __init__(self, pathname):
+        self.target_path = os.path.join(pathname, "target")
+        if not os.path.exists(self.target_path):
+            os.mkdir(self.target_path)
+
+    def init_target_dir(self):
+        if not os.path.exists(self.target_path):
+            os.mkdir(self.target_path)
+        objects_path = os.path.join(self.target_path, "objects")
+        if not os.path.exists(objects_path):
+            os.mkdir(objects_path)
+        backups_path = os.path.join(self.target_path, "backups")
+        if not os.path.exists(backups_path):
+            os.mkdir(backups_path)
+
+    def get_path(self):
+        return self.target_path #volania napr. BackupObject.new...(... , target.get_path())
+
+    def get_backup_path(self, backup_name):
+        backup_path = os.path.join(self.target_path, "backups")
+        return os.path.join(backup_path, backup_name)
+
+    def get_object_path(self, hash):
+        object_path = os.path.join(self.target_path, "objects")
+        return os.path.join(object_path, hash)
+
+    def get_object_header_path(self, hash):
+        object_header_path = os.path.join(self.target_path, "objects")
+        return os.path.join(object_header_path, hash)
+
+    def get_latest_path(self):
+        latest_tmp_path = os.path.join(self.target_path, "backups")
+        return os.path.join(latest_tmp_path, "latest")
+
+    @staticmethod
+    def file_rename(old_name, new_name):
+        new_file_name = os.path.join(os.path.dirname(old_name), new_name)
+        os.rename(old_name, new_file_name)
+
+    def save_file(self, source_path, name, block_size = constants.CONST_BLOCK_SIZE, previous_hash = None):
+        file_hash = hashlib.sha1()
+        with open(source_path, "rb") as SF:
+            target_file = self.get_object_path(name)
+            target_file_header = self.get_object_header_path(name)
+            with gzip.open(target_file, "wb") as TF:
+                while True:
+                    block = SF.read(block_size)
+                    file_hash.update(block)
+                    TF.write(block)
+                    if not block:
+                        self.file_rename(target_file, file_hash.hexdigest() + ".data")
+                        with open(target_file_header, "wb") as THF:
+                            THF.write("gz\n")
+                            THF.write("signature\n")
+                            THF.write(str(0))
+                            THF.write("\n")
+                            self.file_rename(target_file, file_hash.hexdigest() + ".meta")
+                            THF.close()
+                        break
+                TF.close()
+            SF.close()
+        return file_hash.hexdigest()
+
+    def get_object_file(self, hash, mode):
+        return open(self.get_object_path(hash), mode)
+
+    def get_object_file_header(self, hash, mode):
+        return open(self.get_object_header_path(hash), mode)
+
+    def get_object_type(self, hash):
+        with self.get_object_file_header(hash, "rb") as HF:
+            object_type = HF.readline()
+            HF.close()
+            return object_type
+
+    def get_object(self, hash):
+        return StoreObject.create(None, self, None)
+
+    def get_hash(self, src_file, block_size = constants.CONST_BLOCK_SIZE):
+        file_hash = hashlib.sha1()
+        with open(src_file, "rb") as SF :
+            while True:
+                block = SF.read(block_size)
+                file_hash.update(block)
+                if not block : break
+        return file_hash.hexdigest()
+
+
+class StoreObject(BackupObject):
+
+    @staticmethod
+    def create(source_path, store, side_dict):
+        #print side_dict
+        lstat = side_dict['lstat']
+        object_hash = side_dict['hash']
+        object_type = store.get_object_type(object_hash)
+        mode = lstat.st_mode
+        if S_ISDIR(mode) and object_type == "directory":
+            return StoreDir(source_path, store, lstat, side_dict)
+        elif S_ISREG(mode) and object_type == "gz":
+            return StoreGzipFile(source_path, store, lstat, side_dict)
+        elif S_ISREG(mode) and object_type == "raw":
+            return StoreRawFile(source_path, store, lstat, side_dict)
+        elif S_ISREG(mode) and object_type == "delta":
+            return StoreDeltaFile(source_path, store, lstat, side_dict)
+        elif S_ISLNK(mode) and object_type == "link":
+            return StoreLnk(source_path, store, lstat, side_dict)
+        else:
+            # Unknown file
+            return None
+
+    def recovery_stat(self, object_path, lstat):
+        #os.lchmod(object_path, lstat.st_mode)  AttributeError: 'module' object has no attribute 'lchmod'
+        try :
+            time = lstat.st_atime, lstat.st_mtime
+            os.utime(object_path, time)
+        except OSError:
+            pass
+        try:
+            os.chmod(object_path, S_IMODE(lstat.st_mode))
+        except OSError:
+            pass
+        try:
+            os.lchown(object_path, lstat.st_uid, lstat.st_gid)
+        except OSError:
+            pass # doplnit printy / handle exceptetion
+
+    def __init__(self, source_path, store, lstat, side_dict):
+        if objects_init : print("Initializing TargetObject ", source_path)
+        #print source_path
+        BackupObject.__init__(self, source_path, store, lstat)
+        self.side_dict = side_dict
+        #print self.side_dict
+        #print self.name
+
+class StoreFile(StoreObject):
+
+    def __init__(self, source_path, store, lstat, side_dict):
+        if objects_init : print("Initializing TargetFile (%s)") % source_path
+        #print source_path
+        StoreObject.__init__(self, source_path, store, lstat, side_dict)
+
+    def recover(self, block_size = constants.CONST_BLOCK_SIZE):
+        # reverse file_copy()
+        with self.store.get_object_file(self.side_dict['hash'], "rb") as TF:
+            #recovery_file = os.path.join(self.source_path)#name)
+            with open(self.source_path, "wb") as RF:
+                while True:
+                    block = TF.read(block_size)
+                    RF.write(block)
+                    if not block:
+                        break
+                RF.close()
+            TF.close()
+        self.recovery_stat(self.source_path, self.side_dict['lstat'])
+
+class StoreDir(StoreObject):
+
+    #Pomocou tejto metody treba nacitat slovnik objektov v adresari
+    #do vhodnej instancnej premennej objektu triedy TargetDir napriklad v konstruktore.
+    #Do tohto slovnika (nie do side_dict!) potom pristupuje metoda get_object().
+    def __init__(self, source_path, store, lstat , side_dict):
+        if objects_init : print("Initializing TargetDir ", source_path)
+        #print source_path
+        #print target_path
+        #print side_dict
+        #path = os.path.join(target_path, side_dict['hash'])
+        #print path
+        self.loaded_dict = self.unpickling(store.get_object_path(side_dict['hash']))
+        self.loaded_obj = {}
+        StoreObject.__init__(self, source_path, store, lstat, side_dict)
+        #print self.side_dict
+
+    def get_object(self, name):
+        # zisti, ci objekt "name" existuje v zalohovanej verzii
+        # tohto adresara
+        # ak ano, vyrobi prislusny TargetObject
+        # ak nie, vrati None
+        if name in self.loaded_dict:
+            if ('object_' + name) in self.loaded_obj:
+                return self.loaded_obj['object_' + name]
+            else:
+                new_target_object = StoreObject.create(os.path.join(self.source_path, name), self.store, self.loaded_dict[name])
+                self.loaded_obj['object_' + name] = new_target_object
+                return new_target_object
+        else:
+            return None
+
+    def get_object_by_path(self, folders, file_name):
+        size = len(folders)
+        if len(folders) > 0:
+            folder = folders.pop(0)
+            name = folder
+        else:
+            name = file_name
+        if name in self.loaded_dict:
+            if ('object_' + name) in self.loaded_obj:
+                if (name == file_name and size == 0):
+                    return self.loaded_obj['object_' + name]
+                else:
+                    return self.loaded_obj['object_' + name].get_object_by_path(folders, file_name)
+            else:
+                new_store_object = StoreObject.create(os.path.join(self.source_path, name), self.store, self.loaded_dict[name])
+                self.loaded_obj['object_' + name] = new_store_object
+                if (name == file_name and size == 0):
+                    return self.loaded_obj['object_' + name]
+                else:
+                    return self.loaded_obj['object_' + name].get_object_by_path(folders, file_name)
+        else:
+            return None
+
+    def unpickling(self, store_path):
+        #unpkl_file = os.path.join(target_path, file_name)
+        with open(store_path, "rb") as UPF:
+                pi = UPF.read()
+                UPF.close()
+        return_dict = pickle.loads(pi)
+        #print return_dict
+        return return_dict
+
+    def recover(self):
+        #prejst slovnik
+        # ak dir tak rekurzia
+        #inak .recovery_backup
+        #passdef recovery_backup(self):
+        #for name , in self.side_dict.iteritems():
+        # if IS_REG(self.side_dict[key]['lstat'].st_mode):
+        if not os.path.exists(self.source_path):
+            os.mkdir(self.source_path)
+        for store_object_name in self.loaded_dict.iterkeys():
+            new_store_object = self.get_object(store_object_name)
+            new_store_object.recover()#os.path.join(self.source_path, target_object_name))
+        # obnovit metadata adresara!!!!!!!!!!!
+        self.recovery_stat(self.source_path, self.side_dict['lstat'])
+
+class StoreLnk(StoreObject):
+
+    def __init__(self, source_path, store, lstat, side_dict):
+        if objects_init : print("Initializing StoreLnk (%s)") % source_path
+        #print source_path
+        StoreObject.__init__(self, source_path, store, lstat, side_dict)
+
+    def read_backuped_lnk(self):
+        with self.store.get_object_file(self.side_dict['hash'], "rb") as TF:
+            backuped_link_name = TF.read()
+        return backuped_link_name
+
+    def recovery_stat(self, object_path, lstat):
+        try:
+            os.lchown(object_path, lstat.st_uid, lstat.st_gid)
+        except OSError:
+            pass # dolnit printy / handle exceptetion
+
+    def recover(self):
+        os.symlink(self.read_backuped_lnk(), self.source_path )
+        self.recovery_stat(self.source_path, self.side_dict['lstat'])
+
+
+class StoreRawFile(StoreFile, file):
+
+    def __init__(self, source_path, store, lstat, side_dict, file_name):
+        if objects_init : print("Initializing StoreRawFile (%s)") % source_path
+        StoreObject.__init__(self, source_path, store, lstat, side_dict)
+        if type(file_name) == file:
+            self.__dict__.update(file.__dict__)
+        else:
+            file.__init__(self, file_name)
+
+
+class StoreGzipFile(StoreFile, gzip.GzipFile):
+
+    def __init__(self, source_path, store, lstat, side_dict, file_name):
+        if objects_init : print("Initializing StoreGzipFile (%s)") % source_path
+        StoreObject.__init__(self, source_path, store, lstat, side_dict)
+        if type(file_name) == file:
+            self.__dict__.update(file.__dict__)
+        else:
+            gzip.GzipFile.__init__(self, file_name)
+
+
+class StoreDeltaFile(StoreFile, file):
+
+    def __init__(self, source_path, store, lstat, side_dict, file_name):
+        if objects_init : print("Initializing StoreDeltaFile (%s)") % source_path
+        StoreObject.__init__(self, source_path, store, lstat, side_dict)
+        if type(file_name) == file:
+            self.__dict__.update(file.__dict__)
+        else:
+            file.__init__(self, file_name)
